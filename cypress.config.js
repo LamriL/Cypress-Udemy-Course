86dda9263f86f19099286cd74d517e6911c82212
LamriL <lamrinx@msn.com>
2023-11-27 09:28:34 +0100

    24/11 Section 14: API testing finished

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotsFolder: "cypress/reports/mochareports/assets",
   projectId: '786hwi',
+  "reporter": "cypress-multi-reporters",
+  "reporterOptions":{
+    "reporterEnabled": "mochawesome",
+    "mochawesomeReporterOptions":{
+      "reportDir":"cypress/reports/mocha",
+      "quiet": true,
+      "overwrite": false,
+      "html": false,
+      "json": true
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
